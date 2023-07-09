5032f6380763a67a5160e56b369730fc2532bb11
S4ntiagoP <93993799+S4ntiagoP@users.noreply.github.com>
2023-07-09 20:05:28 -0300

    refactor delegation script

--- Delegation/delegation.py
+++ Delegation/delegation.py
@@ -115,31 +115,16 @@ def get_spns( demonID, *params ):
     packer = DelegationPacker()
     demon  = Demon( demonID )
 
-    spn_query = {
-        'spn': '(&(samAccountType=805306368)(!samAccountName=krbtgt)(serviceprincipalname=*)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))',
-        'asrep': '(&(userAccountControl:1.2.840.113556.1.4.803 : = 4194304)(!(UserAccountControl:1.2.840.113556.1.4.803 : = 2)))'
-    }
-
     num_params = len(params)
-    query = ''
-    attributes = ''
+    query = '(&(samAccountType=805306368)(!samAccountName=krbtgt)(serviceprincipalname=*)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))'
+    attributes = 'sAMAccountName,servicePrincipalName'
     result_limit = 0
     hostname = ''
     domain = ''
 
-    if num_params < 1:
-        demon.ConsoleWrite( demon.CONSOLE_ERROR, "Not enough parameters" )
-        return False
-
-    if num_params > 1:
+    if num_params > 0:
         demon.ConsoleWrite( demon.CONSOLE_ERROR, "Too many parameters" )
         return False
-
-    if params[ 0 ].lower() not in spn_query:
-        demon.ConsoleWrite( demon.CONSOLE_ERROR, "Wrong first parameter" )
-        return False
-
-    query = spn_query[ params[ 0 ] ]
 
     # not used
     if num_params >= 2:
@@ -166,5 +151,50 @@ def get_spns( demonID, *params ):
 
     return TaskID
 
+
+def get_asrep( demonID, *params ):
+    TaskID : str    = None
+    demon  : Demon  = None
+    packer = DelegationPacker()
+    demon  = Demon( demonID )
+
+    num_params = len(params)
+    query = '(&(userAccountControl:1.2.840.113556.1.4.803:=4194304)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))'
+    attributes = 'sAMAccountName'
+    result_limit = 0
+    hostname = ''
+    domain = ''
+
+    if num_params > 1:
+        demon.ConsoleWrite( demon.CONSOLE_ERROR, "Too many parameters" )
+        return False
+
+    # not used
+    if num_params >= 2:
+        attributes = params[ 1 ]
+
+    if num_params >= 3:
+        result_limit = params[ 2 ]
+
+    if num_params >= 4:
+        hostname = params[ 3 ]
+
+    if num_params >= 5:
+        domain = params[ 4 ]
+
+    packer.addstr(query)
+    packer.addstr(attributes)
+    packer.adduint32(result_limit)
+    packer.addstr(hostname)
+    packer.addstr(domain)
+
+    TaskID = demon.ConsoleWrite( demon.CONSOLE_TASK, "Tasked demon to run ldap query" )
+
+    demon.InlineExecute( TaskID, "go", f"bin/ldapsearch.{demon.ProcessArch}.o", packer.getbuffer(), False )
+
+    return TaskID
+
+
 RegisterCommand( get_delegation, "", "get-delegation", "Enumerate a given domain for different types of abusable Kerberos Delegation settings.", 0, "[Constrained,Unconstrained,RBCD]", "constrained" )
-RegisterCommand( get_spns, "", "get-spns", "Enumerate a given domain for user accounts with SPNs and ASREP.", 0, "[spn,ASREP]", "spn" )
+RegisterCommand( get_spns, "", "get-spns", "Enumerate a given domain for user accounts with SPNs.", 0, "", "" )
+RegisterCommand( get_asrep, "", "get-asrep", "Enumerate a given domain for user accounts with ASREP.", 0, "", "" )
